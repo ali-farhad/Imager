89761fe803a17cc76f53eef51f1871d47940b732
ali-farhad <alifarhad557@gmail.com>
2022-07-11 13:04:33 -0700

    prepared app for deployment

--- backend/server.js
+++ backend/server.js
@@ -21,46 +21,18 @@ app.use(express.json())
 app.use('/api/', webRoutes)
 app.use('/pdf/', pdfRoutes)
 
+
+//to deploy via backend
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+
+
 //listen for root route on port 5000
 app.listen(5000, () => {
     console.log('Server is running on port 5000')
 })
-
-
-
-// var data =fs.readFileSync('pdfs/' + pdfName);
-// res.contentType("application/pdf");
-// return res.send(data);
-
-  // listTableDocs['content'].push(
-    // //first table
-    //     {
-    //     text: "Order Details",
-    //     style: 'subheader',
-        
-
-    // }, {
-    //     table: table,
-    //     pageBreak: 'after'
-    // },
-    // //2nd table
-    //     {
-    //     text: "Order2 Details",
-    //     style: 'subheader',
-    // }, {
-    //     table: table1,
-    //     pageBreak: 'after'
-    // },
-
-    //  //2nd table
-    //  {
-    //     text: "Order3 Details",
-    //     style: 'subheader',
-    // }, {
-    //     table: table2,
-
-    // },
-
-
-    
-    // )
